09a1cd163fed0102eb31806f7f289f622edc1589
JacksonLaflamme <Jackson.Laflamme@uvm.edu>
2021-03-22 18:44:07 -0400

    finished

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import random
 import pyrosim.pyrosim as pyrosim
 
 length = 1
@@ -30,10 +31,11 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2, linkName="Torso")
     pyrosim.Send_Motor_Neuron(name = 3, jointName="Torso_Back_Leg")
     pyrosim.Send_Motor_Neuron(name = 4, jointName="Torso_Front_Leg")
-    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 3, weight = 0.25)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 0.75)
-    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 4, weight = 0.75)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 4, weight = 0.25)
+    
+    for i in range(0,3):
+        for j in range(3,5):
+            pyrosim.Send_Synapse(sourceNeuronName = i, targetNeuronName = j, weight = (2*random.random())-1)
+
     pyrosim.End()
 
 
