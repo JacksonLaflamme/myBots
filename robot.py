ab98f03f3744b53eb6dad37b04b87ea195eeae48
JacksonLaflamme <Jackson.Laflamme@uvm.edu>
2021-04-18 19:54:46 -0400

    finished deliverable1

--- robot.py
+++ robot.py
@@ -43,9 +43,12 @@ class ROBOT:
         self.nn.Update()
 
     def Get_Fitness(self):
-        stateOfLinkZero=p.getLinkState(self.robot,0)
-        positionOfLinkZero=stateOfLinkZero[0]
-        xCoordinateOfLinkZero = positionOfLinkZero[0]
+        basePositionAndOrientation = p.getBasePositionAndOrientation(self.robot)
+        basePosition = basePositionAndOrientation[0]
+        xCoordinateOfLinkZero = basePosition[2]
+        #stateOfLinkZero=p.getLinkState(self.robot,0)
+        #positionOfLinkZero=stateOfLinkZero[0]
+        #xCoordinateOfLinkZero = positionOfLinkZero[0]
         f = open("tmp"+str(self.myID)+".txt","w")
         f.write(str(xCoordinateOfLinkZero))
         f.close()
