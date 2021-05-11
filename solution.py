09b7b79fed451a5c9e69ec79a4b7f12949d3b75e
JacksonLaflamme <Jackson.Laflamme@uvm.edu>
2021-05-10 20:38:32 -0400

    added hidden layer

--- solution.py
+++ solution.py
@@ -7,7 +7,8 @@ import constants as c
 
 class SOLUTION:
     def __init__(self, nextAvailableID):
-        self.weights = 2*np.random.rand(c.numSensorNeurons,c.numMotorNeurons)-1
+        self.weights_layer_one = 2*np.random.rand(c.numSensorNeurons,c.numHiddenNeurons)-1
+        self.weights_layer_two = 2*np.random.rand(c.numMotorNeurons,c.numHiddenNeurons)-1
         self.myID = nextAvailableID
 
 
@@ -22,7 +23,7 @@ class SOLUTION:
             z = 1.5
 
             pyrosim.Start_SDF("world.sdf")
-            pyrosim.Send_Cube(name="Box", pos = [-400,0,1.5], size = [1000,3.5,3])
+            pyrosim.Send_Cube(name="Box", pos = [-400,0,1.5], size = [1000,3.5,3],mass=-100.0)
             pyrosim.End()
             while not os.path.exists("world.sdf"):
                 time.sleep(0.01)
@@ -76,31 +77,45 @@ class SOLUTION:
             pyrosim.Send_Sensor_Neuron(name = 2, linkName = "Lower_Left_Leg")
             pyrosim.Send_Sensor_Neuron(name = 3, linkName = "Lower_Right_Leg")
             
+            pyrosim.Send_Hidden_Neuron(name = 4)
+            pyrosim.Send_Hidden_Neuron(name = 5)
+            pyrosim.Send_Hidden_Neuron(name = 6)
+            pyrosim.Send_Hidden_Neuron(name = 7)
+            pyrosim.Send_Hidden_Neuron(name = 8)
+            pyrosim.Send_Hidden_Neuron(name = 9)
+            pyrosim.Send_Hidden_Neuron(name = 10)
+            pyrosim.Send_Hidden_Neuron(name = 11)
+            pyrosim.Send_Hidden_Neuron(name = 12)
+            pyrosim.Send_Hidden_Neuron(name = 13)
 
+            pyrosim.Send_Motor_Neuron(name = 14, jointName="Torso_Back_Leg")
+            pyrosim.Send_Motor_Neuron(name = 15, jointName="Torso_Front_Leg")
+            pyrosim.Send_Motor_Neuron(name = 16, jointName = "Torso_Left_Leg")
+            pyrosim.Send_Motor_Neuron(name = 17, jointName = "Torso_Right_Leg")
+            pyrosim.Send_Motor_Neuron(name = 18, jointName = "Back_Leg_Lower_Back_Leg")
+            pyrosim.Send_Motor_Neuron(name = 19, jointName = "Front_Leg_Lower_Front_Leg")
+            pyrosim.Send_Motor_Neuron(name = 20, jointName = "Left_Leg_Lower_Left_Leg")
+            pyrosim.Send_Motor_Neuron(name = 21, jointName = "Right_Leg_Lower_Right_Leg")
 
-            pyrosim.Send_Motor_Neuron(name = 4, jointName="Torso_Back_Leg")
-            pyrosim.Send_Motor_Neuron(name = 5, jointName="Torso_Front_Leg")
-            pyrosim.Send_Motor_Neuron(name = 6, jointName = "Torso_Left_Leg")
-            pyrosim.Send_Motor_Neuron(name = 7, jointName = "Torso_Right_Leg")
-            pyrosim.Send_Motor_Neuron(name = 8, jointName = "Back_Leg_Lower_Back_Leg")
-            pyrosim.Send_Motor_Neuron(name = 9, jointName = "Front_Leg_Lower_Front_Leg")
-            pyrosim.Send_Motor_Neuron(name = 10, jointName = "Left_Leg_Lower_Left_Leg")
-            pyrosim.Send_Motor_Neuron(name = 11, jointName = "Right_Leg_Lower_Right_Leg")
-
-            for currentRow in range(0,c.numSensorNeurons):
-                for currentColumn in range(0, c.numMotorNeurons):
-                    pyrosim.Send_Synapse(sourceNeuronName = currentRow, targetNeuronName = currentColumn+c.numSensorNeurons, weight = self.weights[currentRow][currentColumn])
+            for i in range(0, c.numSensorNeurons):
+                for j in range(0, c.numHiddenNeurons):
+                    pyrosim.Send_Synapse(sourceNeuronName=i ,targetNeuronName=c.numSensorNeurons+j ,weight=self.weights_layer_one[i][j] )
+            
+            for j in range(0, c.numHiddenNeurons):
+                for k in range(0, c.numMotorNeurons):
+                        pyrosim.Send_Synapse(sourceNeuronName=j+c.numSensorNeurons ,targetNeuronName=c.numSensorNeurons+c.numHiddenNeurons+k,weight=self.weights_layer_two[k][j] )
 
             pyrosim.End()
             while not os.path.exists("brain"+str(self.myID)+".nndf"):
                 time.sleep(0.01)
-
+            
 
         Create_World()
         Generate_Body()
         Generate_Brain()
         os.system('start /B python simulate.py '+ arg + " " + time+" "+str(self.myID))
         
+
     def Wait_For_Simulation_To_End(self):
         while not os.path.exists("fitness"+str(self.myID)+".txt"):
             t.sleep(0.01)
@@ -112,8 +127,13 @@ class SOLUTION:
 
     def Mutate(self):
         randomRow = random.randint(0,c.numSensorNeurons-1)
-        randomColumn = random.randint(0,c.numMotorNeurons-1)
-        self.weights[randomRow, randomColumn] = random.random()*2-1
+        randomColumn = random.randint(0,c.numHiddenNeurons-1)
+        self.weights_layer_one[randomRow, randomColumn] = random.random()*2-1
+
+        randomRow = random.randint(0,c.numMotorNeurons-1)
+        randomColumn = random.randint(0,c.numHiddenNeurons-1)
+        self.weights_layer_two[randomRow, randomColumn] = random.random()*2-1
+
 
     def Set_ID(self, ID):
         self.myID = ID
